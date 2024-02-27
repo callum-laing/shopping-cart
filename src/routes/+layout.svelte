1ca5cb42ff8a673bfbaa75a950b4f4dbb4bcd3e7
Callum Laing <callum.laing91@hotmail.co.uk>
2024-02-27 01:54:30 +0000

    added basket icon

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from './Header.svelte';
+    import './global.css';
+	import { page } from '$app/stores';
+	$: path = $page.url.pathname;
+</script>
+
+<Header />
+<slot />
---
+++ src/routes/Header.svelte
@@ -0,0 +1,66 @@
+<header>
+    <div>
+        <h1>Shopping Cart</h1>
+    </div>
+
+<div>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/">Products</a>
+        <a href="/cart">About Us</a>
+    </nav>
+</div>
+<button class="shoppingCart">
+    <span>
+        <img class="cartImage" src="../src/images/shopping-cart.png" alt="">
+    </span>
+</button>
+</header>
+
+
+<style>
+    header {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 1rem;
+}
+
+h1 {
+    color: white;
+}
+
+nav {
+	display: flex;
+	justify-content: center;
+	align-content: center;
+	gap: 50px;
+	padding: 20px;
+}
+
+a {
+	text-decoration: none;
+	color: rgba(200, 200, 250);
+	font-family: 'Fjalla One', sans-serif;
+	letter-spacing: 4px;
+}
+
+a:hover {
+	color: #ccd0e8;
+}
+
+.shoppingCart {
+    background-color: transparent;
+    border: none;
+}
+
+.shoppingCart:hover {
+    cursor: pointer;
+}
+
+.cartImage {
+        filter: invert(100%);
+    }
+
+
+</style>
